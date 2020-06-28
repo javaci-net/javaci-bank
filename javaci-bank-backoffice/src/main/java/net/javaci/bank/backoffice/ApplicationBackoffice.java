3918bb70f7903e6afe9078fb43c8b30aab116d25
ozkan.sari <ozkan.sari@paycore.com>
2020-06-28 17:26:52 +0300

    Javaci Backoffice eklendi

---
+++ javaci-bank-backoffice/src/main/java/net/javaci/bank/backoffice/ApplicationBackoffice.java
@@ -0,0 +1,13 @@
+package net.javaci.bank.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBackoffice {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBackoffice.class, args);
+    }
+
+}
---
+++ javaci-bank-backoffice/src/main/java/net/javaci/bank/backoffice/controller/HomeController.java
@@ -0,0 +1,15 @@
+package net.javaci.bank.backoffice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("welcomeMsg", "Welcome To Javaci-Bank Backoffice");
+        return "home";
+    }
+}
