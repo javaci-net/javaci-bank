e9628daa239a5a95175bb9a94b2780fd0b8c5512
volkanistek <volkanistek@gmail.com>
2020-08-17 04:10:42 +0300

    cors enabled

--- javaci-bank-api/src/main/java/net/javaci/bank/api/config/ApiSecurityConfig.java
+++ javaci-bank-api/src/main/java/net/javaci/bank/api/config/ApiSecurityConfig.java
@@ -56,6 +56,7 @@ public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
                     "/actuator/**").permitAll()
             .anyRequest()
             .authenticated();
+        http.cors();
     }
 
 	@Override
