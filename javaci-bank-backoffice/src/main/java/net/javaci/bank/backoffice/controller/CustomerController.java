089b0c0605ec7edec70299eded6fe31833cda207
volkanistek <volkanistek@gmail.com>
2020-10-02 06:51:48 +0300

    Customer list page

--- javaci-bank-backoffice/src/main/java/net/javaci/bank/backoffice/controller/CustomerController.java
+++ javaci-bank-backoffice/src/main/java/net/javaci/bank/backoffice/controller/CustomerController.java
@@ -1,16 +1,24 @@
 package net.javaci.bank.backoffice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.javaci.bank.db.dao.CustomerDao;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@Autowired
+	private CustomerDao customerDao;
+	
 	@GetMapping("/list")
 	public String renderListPage(Model model) {
+		model.addAttribute("customers", customerDao.findAll());
+		
 		return "customer/list";
 	}
 
