5b2926a4763b636f130b3f949061cba65a267527
volkanistek <volkanistek@gmail.com>
2020-10-04 22:31:27 +0300

    AJAX için ekleme

--- javaci-bank-backoffice/src/main/java/net/javaci/bank/backoffice/controller/CustomerController.java
+++ javaci-bank-backoffice/src/main/java/net/javaci/bank/backoffice/controller/CustomerController.java
@@ -1,12 +1,19 @@
 package net.javaci.bank.backoffice.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import net.javaci.bank.db.dao.CustomerDao;
+import net.javaci.bank.db.model.Customer;
 
 @Controller
 @RequestMapping("/customer")
@@ -21,5 +28,26 @@ public class CustomerController {
 		
 		return "customer/list";
 	}
+	/*
+	
+	@GetMapping("/getByCitizenNumber/{citizenNumber}")
+	@ResponseBody
+	public CustomerListDto getByCitizenNumber(@PathVariable("citizenNumber") String citizenNumber) {
+		
+		final Optional<Customer> customerOptional = customerDao.findByCitizenNumber(citizenNumber);
+		if (!customerOptional.isPresent()) {
+			log.info("Customer not found with citizenNumber:" + citizenNumber);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer couldnt found by citizenNumber: " + citizenNumber);
+		}
+
+		Customer customer = customerOptional.get();
+		
+		return convertToDto(customer);
+	}
+	
+	private CustomerListDto convertToDto(Customer customer) {
+		return modelMapper.map(customer, CustomerListDto.class);
+	}
+	*/
 
 }
