bcff9b0212fab21aabf915b4b97e80f978d5484a
ozkansari <ozkansari@gmail.com>
2020-10-10 23:00:38 +0300

    Model mapper config eklendi tekrar

---
+++ javaci-bank-api/src/main/java/net/javaci/bank/api/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package net.javaci.bank.api.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        final ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
