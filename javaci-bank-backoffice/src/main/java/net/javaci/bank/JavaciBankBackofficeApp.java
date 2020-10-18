76fee76ecf938672eb54d989bdce9ea3fb5963eb
volkanistek <volkanistek@gmail.com>
2020-10-18 05:04:35 +0300

    skipping for spring known bug about throwing SilentExitException when debugging mode https://github.com/spring-projects/spring-boot/issues/3100

--- javaci-bank-backoffice/src/main/java/net/javaci/bank/JavaciBankBackofficeApp.java
+++ javaci-bank-backoffice/src/main/java/net/javaci/bank/JavaciBankBackofficeApp.java
@@ -3,11 +3,24 @@ package net.javaci.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication(scanBasePackages = {"net.javaci.bank.db.dao", "net.javaci.bank.backoffice"})
 public class JavaciBankBackofficeApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaciBankBackofficeApp.class, args);
+        try {
+        	SpringApplication.run(JavaciBankBackofficeApp.class, args);
+        } catch (Throwable e) {
+            if(e.getClass().getName().contains("SilentExitException")) {
+            	// skipping for spring known bug https://github.com/spring-projects/spring-boot/issues/3100
+                log.debug("Spring is restarting the main thread - See spring-boot-devtools");
+            } else {
+                throw e;
+            }
+        }
+        
     }
 
 }
