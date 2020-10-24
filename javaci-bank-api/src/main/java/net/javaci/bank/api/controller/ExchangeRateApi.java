8ff52740b0207c2462555fcf865e3f03e563e683
ozkansari <ozkansari@gmail.com>
2020-10-24 10:20:09 +0300

    exchange rate api duzeltme

--- javaci-bank-api/src/main/java/net/javaci/bank/api/controller/ExchangeRateApi.java
+++ javaci-bank-api/src/main/java/net/javaci/bank/api/controller/ExchangeRateApi.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import io.swagger.annotations.ApiParam;
 import net.javaci.bank.db.dao.ExchangeRateDao;
 import net.javaci.bank.db.model.ExchangeRate;
 
@@ -21,7 +24,16 @@ public class ExchangeRateApi {
 	
 	@GetMapping("/findByLocalDate")
     @ResponseBody
-    public List<ExchangeRate> findByLocalDate(@RequestBody LocalDate date) {
+    public List<ExchangeRate> findByLocalDate(
+    		@ApiParam(value = "Date in yyyy-MM-dd iso date format. Cannot be empty.", required = true, example = "2020-12-31") 
+    		String dateStr
+    ) {
+		LocalDate date = null;
+		try {
+			LocalDate.parse(dateStr);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Date must be in yyyy-MM-dd iso date format. Given input: " + dateStr);
+		}
         return exchangeRateDao.findAllByDate(date);
     }
 }
